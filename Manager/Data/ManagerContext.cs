dfb76c9c2ff71ed63c3ba38f170f457c5dfea4f4
long nguyen <longlop68@gmail.com>
2024-07-25 11:37:39 +0700

    deploy

---
+++ Manager/Data/ManagerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WMK_BE_RecipesAndPlans_DataAccess.Models;
+
+namespace Manager.Data
+{
+    public class ManagerContext : DbContext
+    {
+        public ManagerContext (DbContextOptions<ManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WMK_BE_RecipesAndPlans_DataAccess.Models.Category> Category { get; set; } = default!;
+    }
+}
