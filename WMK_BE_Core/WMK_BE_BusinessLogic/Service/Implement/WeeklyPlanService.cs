fd495cfa237fa14c321ed344a46299bee7e87955
henrykiet <kietyasuo2016@gmail.com>
2024-07-29 17:35:31 +0700

    deploy

--- WMK_BE_Core/WMK_BE_BusinessLogic/Service/Implement/WeeklyPlanService.cs
+++ WMK_BE_Core/WMK_BE_BusinessLogic/Service/Implement/WeeklyPlanService.cs
@@ -266,7 +266,7 @@ namespace WMK_BE_BusinessLogic.Service.Implement
                 return result;
             }
 
-            var weeklyPlans = await _unitOfWork.WeeklyPlanRepository.Get(x => x.ProcessStatus == ProcessStatus.Approved).ToListAsync();
+            var weeklyPlans = await _unitOfWork.WeeklyPlanRepository.GetAllAsync();
             var returnList = weeklyPlans.ToList();
             if (weeklyPlans != null && weeklyPlans.Count > 0)
             {
