a685cc40e59a4fde5385520a355ebf842959b10c
long nguyen <longlop68@gmail.com>
2024-07-29 16:43:33 +0700

    deploy

--- WMK_BE_Core/WMK_BE_BusinessLogic/BusinessModel/ResponseModel/OrderModel/OrderResponse.cs
+++ WMK_BE_Core/WMK_BE_BusinessLogic/BusinessModel/ResponseModel/OrderModel/OrderResponse.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WMK_BE_BusinessLogic.BusinessModel.ResponseModel.CustomPlanModel;
 using WMK_BE_BusinessLogic.BusinessModel.ResponseModel.TransactionModel;
+using WMK_BE_BusinessLogic.BusinessModel.ResponseModel.WeeklyPlanModel;
 using WMK_BE_RecipesAndPlans_DataAccess.Enums;
 using WMK_BE_RecipesAndPlans_DataAccess.Models;
 
@@ -23,6 +24,7 @@ namespace WMK_BE_BusinessLogic.BusinessModel.ResponseModel.OrderModel
 		public DateTime OrderDate { get; set; }
 		public double TotalPrice { get; set; }
 		public string Status { get; set; } = string.Empty;
+		public WeeklyPlanResponseModel weeklyPlan { get; set; }
 		public List<TransactionResponse> Transactions { get; set; }
         //order detail
         public List<OrderDetailResponse> OrderDetails { get; set; }
--- WMK_BE_Core/WMK_BE_RecipesAndPlans_DataAccess/Repository/Implement/OrderRepository.cs
+++ WMK_BE_Core/WMK_BE_RecipesAndPlans_DataAccess/Repository/Implement/OrderRepository.cs
@@ -70,6 +70,7 @@ namespace WMK_BE_RecipesAndPlans_DataAccess.Repository.Implement
             return _dbSet
                 .Where(expression)
                 .Include(o => o.Transactions)
+                .Include(o=>o.WeeklyPlan)
                 .Include(o => o.OrderDetails)
                     .ThenInclude(od => od.Recipe)
                 .Include(o => o.OrderDetails)
