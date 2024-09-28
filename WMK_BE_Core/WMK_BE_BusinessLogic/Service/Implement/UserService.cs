866bef1badb18c8e10e8a6224b17d4b6f80af29c
henrykiet <kietyasuo2016@gmail.com>
2024-09-28 20:17:35 +0700

    deploy

--- WMK_BE_Core/WMK_BE_BusinessLogic/Service/Implement/UserService.cs
+++ WMK_BE_Core/WMK_BE_BusinessLogic/Service/Implement/UserService.cs
@@ -357,34 +357,33 @@ namespace WMK_BE_BusinessLogic.Service.Implement
 				result.Message = "không tìm thấy người dùng";
 				return result;
 			}
-			if ( !userExist.Id.Equals(idUser) )
+
+			if ( !string.IsNullOrEmpty(model.Email) )
 			{
-				if ( !string.IsNullOrEmpty(model.Email) )
+				//check email exist
+				var emailExist = await _unitOfWork.UserRepository.GetByEmailOrUserNameAsync(model.Email);
+				if ( emailExist != null )
 				{
-					//check email exist
-					var emailExist = await _unitOfWork.UserRepository.GetByEmailOrUserNameAsync(model.Email);
-					if ( emailExist != null )
-					{
-						result.StatusCode = 400;
-						result.Message = "Email này đã được đăng kí";
-						return result;
-					}
-					userExist.Email = model.Email;
-					userExist.EmailConfirm = EmailConfirm.NotConfirm;
+					result.StatusCode = 400;
+					result.Message = "Email này đã được đăng kí";
+					return result;
 				}
-				if ( !string.IsNullOrEmpty(model.UserName) )
-				{
-					//check username exist
-					var usernameExist = await _unitOfWork.UserRepository.GetByEmailOrUserNameAsync(model.UserName);
-					if ( usernameExist != null )
-					{
-						result.StatusCode = 400;
-						result.Message = "Tên người dùng đã tồn tại";
-						return result;
-					}
-					userExist.UserName = model.UserName;
-				}
+				userExist.Email = model.Email;
+				userExist.EmailConfirm = EmailConfirm.NotConfirm;
 			}
+			if ( !string.IsNullOrEmpty(model.UserName) )
+			{
+				//check username exist
+				var usernameExist = await _unitOfWork.UserRepository.GetByEmailOrUserNameAsync(model.UserName);
+				if ( usernameExist != null )
+				{
+					result.StatusCode = 400;
+					result.Message = "Tên người dùng đã tồn tại";
+					return result;
+				}
+				userExist.UserName = model.UserName;
+			}
+
 			if ( !string.IsNullOrEmpty(model.Phone) )
 			{
 				userExist.Phone = model.Phone;
